5af35fb5d26328cdbc3de6840117392f7d93c612
LewFos <173790050+LewFos@users.noreply.github.com>
2024-09-05 18:40:34 +0900

    fuck me

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(dildo) {
 
+    let odlid = "";
+
+    for (i = 0; i < dildo.length; i++) {
+        odlid = dildo.charAt(i) + odlid;
+      }
+
+    return odlid;
+    
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+         
