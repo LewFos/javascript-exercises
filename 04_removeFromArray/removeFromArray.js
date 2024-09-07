52b652a4cefeecb9d6fe2c88cbb0ed0ce51736fa
LewFos <173790050+LewFos@users.noreply.github.com>
2024-09-08 08:03:13 +0900

    my balls are sore

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(fucksGiven, ...howManyFucks) {
+
+    const fucksTaken = [];
+    
+    fucksGiven.forEach((fuck) => {
+      
+      if (!howManyFucks.includes(fuck)) {
+        fucksTaken.push(fuck);
+      }
+    });
+    
+    return fucksTaken;
+
+
 };
 
 // Do not edit below this line
