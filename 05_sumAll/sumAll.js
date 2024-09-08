4408481ca99d2f3d2a300a98d7a665c70f32f398
LewFos <173790050+LewFos@users.noreply.github.com>
2024-09-09 07:45:30 +0900

    i like things

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,39 @@
-const sumAll = function() {
+const sumAll = function(alpha, omega) {
+
+    let eatShit = omega;
+    let b = Number.isInteger(alpha);
+    let c = Number.isInteger(omega);
+
+ 
+    if (b != true || c != true || alpha < 0) {
+
+        eatShit = "ERROR";
+
+    } else if (alpha > omega) {
+
+
+        for (let i = alpha; i > omega; i--) {
+
+            eatShit = eatShit + i;
+
+        }
+
+        
+    } else
+        
+        
+        
+    {
+
+        for (let i = alpha; i < omega; i++) {
+
+            eatShit = eatShit + i;
+
+        }
+
+    }
+
+    return eatShit;
 
 };
 
