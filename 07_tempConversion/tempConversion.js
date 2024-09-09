d572e8e20aa5ea6da5919d224855d9c37e3d936f
LewFos <173790050+LewFos@users.noreply.github.com>
2024-09-09 18:34:54 +0900

    i think my hands are made out of wood

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(rectalThermometer) {
+
+  let anusTemp = (rectalThermometer - 32) * 5 / 9;
+
+  let spermBurp = Math.round(anusTemp * 10.0) / 10.0;;
+  
+  return spermBurp;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(rectalThermometer) {
+
+  let anusTemp = rectalThermometer * (9 / 5) + 32;
+
+  let spermBurp = Math.round(anusTemp * 10.0) / 10.0;;
+  
+  return spermBurp;
+
 };
 
 // Do not edit below this line
